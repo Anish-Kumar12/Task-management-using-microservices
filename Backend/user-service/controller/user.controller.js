6aec48cb8c05ac5f380326df9162ae887e83d949
Anish Kumar <anishkumar344567@gmail.com>
2025-08-21 11:42:43 +0530

    added getalluser route

--- Backend/user-service/controller/user.controller.js
+++ Backend/user-service/controller/user.controller.js
@@ -10,3 +10,13 @@ export const saveUser  = async (req, res) => {
     }
 };
 
+export const getAllUser = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
--- Backend/user-service/routes/user.route.js
+++ Backend/user-service/routes/user.route.js
@@ -1,8 +1,9 @@
-import { saveUser } from "../controller/user.controller.js";
+import { saveUser , getAllUser} from "../controller/user.controller.js";
 
 import express from 'express';
 const router = express.Router();
 
 router.post('/save', saveUser);
+router.get('/getAll', getAllUser);
 
 export default router;
