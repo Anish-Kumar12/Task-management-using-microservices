d91087edf99b511616115120e8b4af6c9fdd425c
Anish Kumar <anishkumar344567@gmail.com>
2025-08-21 11:28:53 +0530

    transferred the local uri to env

--- Backend/user-service/Database/db.connect.js
+++ Backend/user-service/Database/db.connect.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/users');
+    await mongoose.connect(`${process.env.MONGO_URI}`);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
