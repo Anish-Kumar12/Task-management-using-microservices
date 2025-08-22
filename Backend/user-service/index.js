d5b0c8e5acbf16ab7be633f7e6d232de294837f6
Anish Kumar <anishkumar344567@gmail.com>
2025-08-22 09:02:47 +0530

    debugging

--- Backend/user-service/index.js
+++ Backend/user-service/index.js
@@ -1,12 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import bodyparser from 'body-parser';
 import connectDB from './Database/db.connect.js';
 import userRoutes from './routes/user.route.js';
 dotenv.config();
 
 const app = express();
-app.use(bodyparser.json());
 app.use(express.json());
 app.use('/user', userRoutes);
 
